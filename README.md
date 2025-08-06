e78e6de76e2ccacd0690bc662b122271d67dfb8a
egtechgeek <105953082+egtechgeek@users.noreply.github.com>
2025-08-06 15:35:51 -0400

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# TARR_Annunciator
+# TARR Annunciator
+
+-Designed to be run on a Raspberry Pi 4b
+
+-You will encounter errors if you attempt to run install.sh more than once
+-Update.sh will poll the repository for updated files, and copy them into the install directory.
+
+## Prerequisites 
+
+-Pi audio output set to 3.5mm
+-Pi advanced audio set to pulseaudio
+
---
+++ update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This script will update the TARR Annunciator application to the latest version.
+# It must be run as root.
+
+# Exit on any error
+set -e
+
+# Check if running as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root!"
+    exit 1
+fi
+
+echo "Updating TARR Annunciator..."
+
+# Clone the latest version from GitHub
+echo "Cloning the latest version of TARR Annunciator..."
+git clone https://egtechgeek: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@github.com/egtechgeek/TARR_Annunciator/ /tmp/TARR_Annunciator
+
+# Copy necessary files and directories, overwriting the old ones
+echo "Copying updated files to the Annunciator directory..."
+
+cp -r /tmp/TARR_Annunciator/templates /opt/TARR_Annunciator/templates
+cp -r /tmp/TARR_Annunciator/static /opt/TARR_Annunciator/static
+cp /tmp/TARR_Annunciator/app.py /opt/TARR_Annunciator/app.py
+cp /tmp/TARR_Annunciator/install.sh /opt/TARR_Annunciator/install.sh
+cp /tmp/TARR_Annunciator/update.sh /opt/TARR_Annunciator/update.sh
+
+# Set the executable permissions on the install and update scripts
+chmod +x /opt/TARR_Annunciator/install.sh
+chmod +x /opt/TARR_Annunciator/update.sh
+
+# Clean up the temporary directory
+rm -rf /tmp/TARR_Annunciator
+
+echo "TARR Annunciator has been successfully updated!"
